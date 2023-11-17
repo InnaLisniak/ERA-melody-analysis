02c3fa2f71674fa9244639c879ffde9fc6a497e2
InnaLisniak <150320008+InnaLisniak@users.noreply.github.com>
2023-11-17 08:28:22 +0200

    Create table_of_repetitions_of_the_last_3_notes_of_the_highest_10.py
    a counting table of the most frequently used last three notes and the largest 10 values are displayed

---
+++ table_of_repetitions_of_the_last_3_notes_of_the_highest_10.py
@@ -0,0 +1,14 @@
+# Convert lists to tuples
+df['LastThreeNumbers'] = df['LastThreeNumbers'].apply(tuple)
+
+# Create a pivot table-like result using groupby and count
+pivot_table = df.groupby('LastThreeNumbers').size().reset_index(name='Count')
+sorted_pivot_table = pivot_table.sort_values(by='Count', ascending=False)
+
+# Display the sorted pivot table
+#print(sorted_pivot_table)
+
+LastThreeNumbers_values = sorted_pivot_table.head(10)
+
+# Display the eight largest values
+print(LastThreeNumbers_values)
